3d4d27e5739c5b0d00a9c9c21f3f0167c04cc4bf
shiyu <1020630835@qq.com>
2020-07-04 22:44:20 +0800

    登录页面

---
+++ vue-admin/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+
+}
+
+
+</style>
---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        登录页面
+    </div>
+</template>
+
+<script lang="scss" scope>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+#login{
+    height:100vh;
+    background-color:#344a57;
+}
+</style>
